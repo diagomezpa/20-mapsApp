a31fd1407b189a104a91a10b9ad4951098c34cab
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-10-08 16:03:32 -0500

    seccion 21 class 288 aplication menu

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name: string;
+  route: string;
+}
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
-
+  menuItems: MenuItem[] = [
+    { name: 'Full-Screen', route: '/maps/fullscreen' },
+    { name: 'Markers', route: '/maps/markers' },
+    { name: 'Properties', route: '/maps/properties' },
+    { name: 'Zoom-Range', route: '/maps/zoom-range' }
+  ];
 }
